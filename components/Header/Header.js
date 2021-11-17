f4641c4ded6c6b9eb1193c32dfd4aef5d8ea0694
David <david.llobet6@gmail.com>
2021-11-17 18:42:09 +0100

    Create Header component

---
+++ components/Header/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header className="d-flex justify-content-center py-3">
+      <Link className="nav-link" href="/">
+        HOME/
+      </Link>
+      {/* <Link className="nav-link" href="/Tuits">
+        TUITS/
+      </Link>
+      <Link className="nav-link" href="/Crear Tuit">
+        CREAR TUIT/
+      </Link> */}
+    </header>
+  );
+};
+
+export default Header;
